f9656e5fc56478ddc18e0aef04642c1bcfc28c31
pigfyy <111782362+pigfyy@users.noreply.github.com>
2025-05-10 21:42:50 -0700

    completed m05

---
+++ m05/script.js
@@ -0,0 +1,13 @@
+// use prompt() to get number
+const table = parseInt(prompt("Enter a number for the multiplication table:"));
+
+var msg = "<h2>Multiplication Table</h2>";
+
+// generate multiplication table
+for (let i = 1; i <= 10; i++) {
+  msg += i + " x " + table + " = " + i * table + "<br />";
+}
+
+// display the generated multiplication table on the blackboard
+const el = document.getElementById("blackboard");
+el.innerHTML = msg;
